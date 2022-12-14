3335a1aa5c7279a374636d4a5bd33d980be46468
German Velardez <germanvelardez16@gmail.com>
2022-12-14 12:57:12 -0300

    Comienzo version 3.0.0. Enfocado en simplificar aplicacion y optimizar el consumo

---
+++ runCode.sh
@@ -0,0 +1,20 @@
+# Script to flash device with files from build folder
+# Author: German Velardez     (germanvelardez16@gmail.com)
+#
+#
+echo -e "making somethings ... \n\t\t"
+make
+
+#file= $(ls ./build | grep .hex)
+
+echo -e "\n\n"
+#read -p "introduce the name of the file .hex what you want to flash: " file
+
+echo "flash the device..."
+openocd -f interface/stlink.cfg -f target/stm32f1x.cfg -c "program build/$(ls ./build | grep .hex) verify reset"
+
+
+# Leer memoria
+#read_memory 0xaddress numerodebits(8,16,32) cantidadaleer
+#Escribir memoria
+# write_memory 0xaddress numerodebits(8,16,32) {datos1,dato2}
